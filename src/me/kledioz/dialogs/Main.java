74b8688a89d462a2ebb43068303faf50e030f8e9
Kledioz <claudiobo1122@gmail.com>
2020-05-06 18:35:06 -0700

    Mas comandos, mensajes faltantes, Relacionar tasks con jugadores.

--- src/me/kledioz/dialogs/Main.java
+++ src/me/kledioz/dialogs/Main.java
@@ -3,6 +3,7 @@ package me.kledioz.dialogs;
 import java.util.HashMap;
 import java.util.List;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -10,20 +11,23 @@ public class Main extends JavaPlugin {
 
 	Main pl;
 	HashMap<String, List<String>> dialogos;
-	HashMap<Integer, BukkitTask> tasks;
+	HashMap<Player, BukkitTask> tasks;
 
 	public void onEnable() {
 		pl = this;
 		dialogos = new HashMap<String, List<String>>();
-		tasks = new HashMap<Integer, BukkitTask>();
+		tasks = new HashMap<Player, BukkitTask>();
 		saveDefaultConfig();
 		reloadConfig();
 		getCommand("tdialogs").setExecutor(new DialogCommand(this));
-
+		loadDialogs();
+	}
+	
+	public void loadDialogs() {
+		dialogos.clear();
 		for (String name : getConfig().getConfigurationSection("dialogs").getKeys(false)) {
 			dialogos.put(name.toLowerCase(), getConfig().getStringList("dialogs." + name));
 		}
-		
 	}
 
 	public void onDisable() {
